85e1f63fad8d355dca5d5a3fdc1ffc85377e45a5
denkleman <daniyar.urazbayev99@gmail.com>
2019-01-21 09:29:50 +0600

    commit

--- 1.py
+++ 1.py
@@ -1,5 +1,12 @@
 def hehe():
-    for i in range(1,101):
-        print(i)
+    a = []
+
+    for i in range(0,101):
+        if i%2==0:
+        
+            a.append(i)
+    print(a)
+        
+
 
 hehe()
